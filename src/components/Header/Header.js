c5776a5c230ec0323c6d1109a4817bc4329582c4
EgorStogov <egst94@yandex.ru>
2022-08-10 22:53:10 +0300

    feature: add layout for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,16 @@
+import headerLogo from "../../images/logo.svg";
+
 function Header() {
   return(
-    <></>
+    <section className="header">
+      <button className="header__button">
+        <img className="header__logo" src={headerLogo} alt="Логос" />
+      </button>
+      <div className="header__auth">
+        <button className="header__button header__button_type_register">Регистрация</button>
+        <button className="header__button header__button_type_signin">Войти</button>
+      </div>
+    </section>
   );
 }
 
