14d198420d5e87fb81475649907f7f011d279491
EgorStogov <egst94@yandex.ru>
2022-08-12 00:29:30 +0300

    feature: add simple logic for Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,18 @@
-function Navigation() {
+function Navigation(isLoggedIn) {
   return(
-    <></>
+    <>
+      {isLoggedIn ? 
+        <div className="header__auth">
+          <button className="header__button header__button_type_register">Регистрация</button>
+          <button className="header__button header__button_type_signin">Войти</button>
+        </div>
+                  :
+        <div className="header__auth">
+          <button className="header__button header__button_type_register">Регистрация</button>
+          <button className="header__button header__button_type_signin">Войти</button>
+        </div>
+      }
+    </>
   );
 }
 
