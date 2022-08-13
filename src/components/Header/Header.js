2a62c37dc3355efed9a023568bc0878989e68895
EgorStogov <egst94@yandex.ru>
2022-08-13 21:34:47 +0300

    refactor: replace Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ function Header() {
       <button className="header__button">
         <img className="header__logo" src={headerLogo} alt="Логос" />
       </button>
-      <Navigation />
+      <div className="header__auth">
+          <button className="header__button header__button_type_register">Регистрация</button>
+          <button className="header__button header__button_type_signin">Войти</button>
+        </div>
     </section>
   );
 }
