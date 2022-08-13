84e36b9d038556b67afbaff74e8974ad472d37dd
EgorStogov <egst94@yandex.ru>
2022-08-13 17:58:18 +0300

    feature: add layout for Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,30 @@
+import "./Portfolio.css";
+
 function Portfolio() {
   return(
-    <></>
+    <section className="portfolio">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <ul className="portfolio__links-list">
+        <li className="portfolio__link-container">
+          <a className="portfolio__link" href="https://eastogov.github.io/how-to-learn/">
+            <h4 className="portfolio__link-title">Статичный сайт</h4>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+        </li>
+        <li className="portfolio__link-container">
+          <a className="portfolio__link" href="https://github.com/EAStogov/russian-travel">
+            <h4 className="portfolio__link-title">Адаптивный сайт</h4>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+        </li>
+        <li className="portfolio__link-container">
+          <a className="portfolio__link" href="https://esto.mesto.nomoredomains.xyz/">
+            <h4 className="portfolio__link-title">Одностраничное приложение</h4>
+            <p className="portfolio__link-icon">↗</p>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 }
 
