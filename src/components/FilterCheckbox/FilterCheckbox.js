217db25ba022fc0106623fdac99a5f6bef016dc7
EgorStogov <egst94@yandex.ru>
2022-08-15 20:14:21 +0300

    feature: add checkbox

---
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./FilterCheckbox.css";
+
+function FilterCheckbox() {
+  const [isChecked, setIsChecked] = useState(true);
+  function toggleCheck() {
+    setIsChecked(!isChecked);
+  }
+  return(
+    <div className="filter">
+      <button className={`filter__checkbox ${isChecked && 'filter__checkbox_active'}`} onClick={toggleCheck}>
+        <div className={`filter__checkbox-circle ${isChecked && 'filter__checkbox-circle_active'}`}></div>
+      </button>
+      <p className="filter__title">Короткометражки</p>
+    </div>
+  );
+}
+
+export default FilterCheckbox;
