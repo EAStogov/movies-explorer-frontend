6c84ce44bac1e35df1c5769fc4e2663e8ada76ee
EgorStogov <egst94@yandex.ru>
2022-08-15 20:20:14 +0300

    feature: add layout & styles for MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,8 +1,24 @@
 import "./MoviesCardList.css";
+import MoviesCard from "../MoviesCard/MoviesCard";
 
-function MoviesCardList() {
+function MoviesCardList({moviesList}) {
   return(
-    <></>
+    <section className="movies">
+      <ul className="movies__card-list">
+        {moviesList.map(movie => {
+          return (
+            <MoviesCard 
+              name={movie.name} 
+              duration={movie.duration}
+              image={movie.image}
+              key={movie.id}
+              saved={movie.saved}
+            />
+          )
+        })}
+      </ul>
+      <button className="movies__more">Еще</button>
+    </section>
   );
 }
 
