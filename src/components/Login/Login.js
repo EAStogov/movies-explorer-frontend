58df703e86c9e121a69525dead1e8ddaa981aebc
EgorStogov <egst94@yandex.ru>
2022-08-16 13:32:07 +0300

    feature: add login's comp layout & styles

---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Form from "../Form/Form";
+import "./Login.css";
+
+function Login() {
+  const [email, setEmail] = useState('pochta@yandex.ru');
+  const [password, setPassword] = useState('');
+
+  function handleChangeEmail(e) {
+    setEmail(e.target.value)
+  }
+
+  function handleChangePassword(e) {
+    setPassword(e.target.value)
+  }
+
+  return(
+    <section className="login">
+      <Form title="Рады видеть!" submitText="Войти" navLinkText="Еще не зарегистрированы?" navLinkButtonText="Регистрация">
+         <div className="form__input-container">
+          <p className="form__input-label">E-mail</p>
+          <input className="form__input" value={email} onChange={handleChangeEmail}/>
+        </div>
+        <div className="form__input-container">
+          <p className="form__input-label">Пароль</p>
+          <input className="form__input form__input_invalid" type="password" value={password} onChange={handleChangePassword}/>
+        </div>
+      </Form>
+    </section>
+  );
+}
+
+export default Login;
