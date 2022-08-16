e594a5b23566b2e51b8e183ba7c02434acc9624f
EgorStogov <egst94@yandex.ru>
2022-08-16 17:39:31 +0300

    refactor: refactor footer & navigation

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,17 @@ function Footer({ isFooterShown }) {
       <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
       <div className="footer__container">
         <p className="footer__year">© 2022</p>
-        <Navigation />
+        <ul className="footer__nav">
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="https://practicum.yandex.ru" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+          </li>
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="https://github.com/EAStogov" target="_blank" rel="noreferrer">Github</a>
+          </li>
+          <li className="footer__nav-item">
+            <a className="footer__nav-link" href="https://vk.com/id15985072" target="_blank" rel="noreferrer">VK</a>
+          </li>
+        </ul>
       </div>
     </section>
   );
