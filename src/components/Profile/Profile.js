d7e75eadcaa787081a67fcf88c3e254cc29d6772
EgorStogov <egst94@yandex.ru>
2022-08-16 13:30:45 +0300

    feature: add profile's comp layout & styles

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import Form from "../Form/Form";
-import ValidativeInput from "../ValidativeInput/ValidativeInput";
 import "./Profile.css";
 
 function Profile() {
+  const [name, setName] = useState('Виталий');
+  const [email, setEmail] = useState('pochta@yandex.ru');
+
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+
+  function handleChangeEmail(e) {
+    setEmail(e.target.value)
+  }
+
   return(
     <section className="profile">
-      <h2 className="profile__title">Привет, Виталий</h2>
-      <Form isForEdit={true} navLink="/">
-        <ValidativeInput type="email" />
+      <Form isForEdit={true} navLink="/" title="Привет, Виталий!" submitText="Редактировать" navLinkButtonText="Выйти из аккаунта">
+        <div className="form__input-container form__input-container_type_edit">
+          <p className="form__input-label form__input-label_type_edit">Имя</p>
+          <input className="form__input form__input_type_edit" value={name} onChange={handleChangeName}/>
+        </div>
+        <div className="form__input-container form__input-container_type_edit">
+          <p className="form__input-label form__input-label_type_edit">E-mail</p>
+          <input className="form__input form__input_type_edit" value={email} onChange={handleChangeEmail}/>
+        </div>
       </Form>
     </section>
   );
