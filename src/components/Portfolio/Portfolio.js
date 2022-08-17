436742305472be6a9a45aa65ea824c7518aca95a
EgorStogov <egst94@yandex.ru>
2022-08-17 22:34:33 +0300

    refactor: add links opennig in new tab

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,19 +6,34 @@ function Portfolio() {
       <h3 className="portfolio__title">Портфолио</h3>
       <ul className="portfolio__links-list">
         <li className="portfolio__link-container">
-          <a className="portfolio__link" href="https://eastogov.github.io/how-to-learn/">
+          <a
+            className="portfolio__link"
+            href="https://eastogov.github.io/how-to-learn/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <h4 className="portfolio__link-title">Статичный сайт</h4>
             <p className="portfolio__link-icon">↗</p>
           </a>
         </li>
         <li className="portfolio__link-container">
-          <a className="portfolio__link" href="https://github.com/EAStogov/russian-travel">
+          <a
+            className="portfolio__link"
+            href="https://eastogov.github.io/russian-travel/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
             <h4 className="portfolio__link-title">Адаптивный сайт</h4>
             <p className="portfolio__link-icon">↗</p>
           </a>
         </li>
         <li className="portfolio__link-container">
-          <a className="portfolio__link" href="https://esto.mesto.nomoredomains.xyz/">
+          <a
+            className="portfolio__link"
+            href="https://esto.mesto.nomoredomains.xyz/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <h4 className="portfolio__link-title">Одностраничное приложение</h4>
             <p className="portfolio__link-icon">↗</p>
           </a>
