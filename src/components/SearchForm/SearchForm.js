e186a38c157861b66e247c35c3c1d81e8abadb1c
EgorStogov <egst94@yandex.ru>
2022-08-17 23:55:51 +0300

    feature: add attribyte 'required' for search input

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm() {
     <section className="search">
       <form className="search__form">
         <img className="search__icon" src={searchIcon} alt="Поиск" />
-        <input className="search__input" placeholder="Фильм" />
+        <input className="search__input" placeholder="Фильм" required />
         <button type="submit" className="page__button search__submit-button"></button>
       </form>
       <FilterCheckbox />
