b7153a3fc3be94d54863da80950f5a8f2826b8a9
EgorStogov <egst94@yandex.ru>
2022-10-31 21:54:28 +0300

    feature: add navigation for trailerLink

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -15,9 +15,8 @@ function MoviesCard({ name, duration, image, saved, movie, handleLike, handleDis
     }
   }, [])
 
-  
-
-  function toggleLike() {
+  function toggleLike(e) {
+    e.preventDefault();
     if (!isSaved) {
       Promise.resolve(handleLike(movie))
         .then(() => {
@@ -32,18 +31,20 @@ function MoviesCard({ name, duration, image, saved, movie, handleLike, handleDis
   }
   return (
     <li className="movie-card">
-      <div className="movie-card__container">
-        <h2 className="movie-card__title">{name}</h2>
-        <p className="movie-card__duration">{duration < 60 ? `${duration}м` : `${Math.floor(duration / 60)}ч ${duration % 60}м` }</p>
-        <button
-          type="button"
-          className={`page__button movie-card__like ${isSaved && "movie-card__like_active"} ${
-            window.location.href === "http://localhost:3000/saved-movies" && "movie-card__remove"
-          }`}
-          onClick={toggleLike}
-        ></button>
-      </div>
-      <img className="movie-card__image" src={image} alt={name} />
+      <a className="movie-card__link" href={movie.trailerLink} rel="noopener noreferrer" target="_blank">
+        <div className="movie-card__container">
+          <h2 className="movie-card__title">{name}</h2>
+          <p className="movie-card__duration">{duration < 60 ? `${duration}м` : `${Math.floor(duration / 60)}ч ${duration % 60}м` }</p>
+          <button
+            type="button"
+            className={`page__button movie-card__like ${isSaved && "movie-card__like_active"} ${
+              window.location.href === "http://localhost:3000/saved-movies" && "movie-card__remove"
+            }`}
+            onClick={toggleLike}
+          ></button>
+        </div>
+        <img className="movie-card__image" src={image} alt={name} />
+      </a>
     </li>
   );
 }
